e72d45acc26c6543ac4bf668f455527175d75f15
Elaina <1463967532@qq.com>
2025-04-09 00:29:16 +0800

    refactor(docker_start.sh)现在如果有install build log目录不会删除再重新构建

--- docker_start.sh
+++ docker_start.sh
@@ -3,12 +3,12 @@ last_dir=$(basename $(pwd))
 echo "当前目录是: $last_dir"
 cd ./ros_ws
 # 如果有install、build、log目录则删除
-for dir in install build log devel; do
-    if [ -d "$dir" ]; then
-        echo "删除目录: $dir"
-        rm -rf "$dir"
-    fi
-done
+# for dir in install build log devel; do
+#     if [ -d "$dir" ]; then
+#         echo "删除目录: $dir"
+#         rm -rf "$dir"
+#     fi
+# done
 #绕过交互式检查,通过-i选项强制bash以交互模式运行
 
 bash -i -c "source ~/.bashrc;catkin_make"
