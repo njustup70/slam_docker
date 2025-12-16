249600aa21818b8b38637a60aa9f68120b040950
Elaina <1463967532@qq.com>
2025-12-16 21:33:00 +0800

    更改voxel_slam仓库

--- VoxelSlam/.devcontainer/build.bash
+++ VoxelSlam/.devcontainer/build.bash
@@ -7,8 +7,8 @@ SCRIPT_DIR=$(dirname "$0")
 TAG="voxel_slam"
 
 # 从外部传入的 IMAGE_REPO（格式：ghcr.io/user/repo 或 docker.io/user/repo）
-IMAGE_REPO=${IMAGE_REPO:-elainasuki/rc2025}
-
+# IMAGE_REPO=${IMAGE_REPO:-elainasuki/rc2025}
+IMAGE_REPO=elainasuki/rc2026
 # 组合完整镜像名
 IMAGE="$IMAGE_REPO:$TAG"
 
